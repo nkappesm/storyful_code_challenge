ae8bb0b84cb68d6ef52286bfc7efa651560fe484
Map <nkappesm@gmail.com>
2022-05-13 11:01:16 +0100

    Refactor basket methods

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,13 +1,20 @@
 class Basket
+  DISCOUNTS = Hash[
+    '1': 0,
+    '2': 0.05,
+    '3': 0.1,
+    '4': 0.2,
+    '5': 0.25
+  ]
 
-  def initialize()
+  def initialize
     @books = {}
   end
 
   def total_price
     temporal_basket = @books.dup
     result = 0
-    while temporal_basket.length > 0
+    while temporal_basket.length.positive?
       new_set = book_set(temporal_basket)
       discount = discount_amount(new_set)
       result += set_price(new_set, discount)
@@ -36,20 +43,9 @@ class Basket
   end
 
   def discount_amount(book_set)
-    case book_set.length
-    when 1
-      0.0
-    when 2
-      0.05
-    when 3
-      0.1
-    when 4
-      0.2
-    when 5
-      0.25
-    else
-      raise(StandardError, 'Invalid amount of books in the Series')
-    end
+    set_length = book_set.length
+    raise(StandardError, 'Invalid amount of books in the Series') unless set_length.positive? && set_length <= 5
+    DISCOUNTS[:"#{set_length}"]
   end
 
   def set_price(book_set, discount)
